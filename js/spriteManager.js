76ffafd6351af56777600489ffbb1dcf8a9e4062
DrMothra <tony.glover@nottingham.ac.uk>
2015-01-28 16:10:27 +0000

    Sprite management

---
+++ js/spriteManager.js
@@ -0,0 +1,12 @@
+/**
+ * Created by atg on 28/01/2015.
+ */
+//Manage all the labels (sprites) for the website
+
+function spriteManager() {
+    var labels = [];
+
+    create: function(name, position, scale, colour, fontSize, opacity, visible) {
+
+    }
+}
